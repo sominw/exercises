c36cf661a8ab9508ad9293d8819c60adde4bbc0c
sominwadhwa <sominwadhwa@gmail.com>
2019-01-04 11:02:08 +0530

    insertion impl

--- src/linkedlist/BasicImpl.java
+++ src/linkedlist/BasicImpl.java
@@ -1,24 +1,61 @@
 package linkedlist;
 
-class Node {
-	int data;
-	Node next = null;
+import linkedlist.LinkedList.Node;
 
-	public Node(int data) {
-		this.data = data;
+class LinkedList {
+
+	Node head;
+	
+	public void insertElement(int element) {
+		Node tempHead = this.head;
+		if (tempHead == null) {
+			this.head = new LinkedList.Node(element);
+			System.out.println("Successfully inserted element into new list.");
+			return;
+		}
+		int count = 1;
+		while (tempHead.next != null) {
+			count++;
+			tempHead = tempHead.next;
+		}
+		Node node = new LinkedList.Node(element);
+		tempHead.next = node;
+		System.out.println("Successfully inserted element at index: " + count);
 	}
+	
+	public void printList() {
+		Node tempHead = this.head;
+		if (tempHead == null) {
+			System.out.println("Empty list!");
+			return;
+		}
+		System.out.print("Current List -> ");
+		while (tempHead != null) {
+			System.out.print(tempHead.data + "\t");
+			tempHead = tempHead.next;
+		}
+	}
+
+	static class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
 }
 
 public class BasicImpl {
 	public static void main(String[] args) {
 
-		Node head = new Node(1);
-		head.next = new Node(2);
-		head.next.next = new Node(3);
-
-		while (head != null) {
-			System.out.println(head.data);
-			head = head.next;
-		}
+		LinkedList list = new LinkedList();
+		list.insertElement(1);
+		list.insertElement(2);
+		list.insertElement(3);
+		list.insertElement(4);
+		list.printList();
 	}
 }
