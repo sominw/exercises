07f64821776de34563e452e77fd04634d040bd4a
sominwadhwa <sominwadhwa@gmail.com>
2019-01-04 12:59:24 +0530

    delete by index

--- src/linkedlist/BasicImpl.java
+++ src/linkedlist/BasicImpl.java
@@ -3,7 +3,24 @@ package linkedlist;
 class LinkedList {
 
 	Node head;
-	
+
+	public void deleteByIndex(int index) {
+		if (this.head == null || index + 1 > this.getSize() || index < 0) {
+			System.out.println("Could not delete. Either list is empty or index is invalid");
+			return;
+		}
+		Node tempNode = this.head;
+		Node prevNode = null;
+		int count = 0;
+		while (count != index) {
+			count++;
+			prevNode = tempNode;
+			tempNode = tempNode.next;
+		}
+		System.out.println("Element deleted at index: " + index);
+		prevNode.next = tempNode.next;
+	}
+
 	public void reverseList() {
 		if (this.head == null || this.getSize() <= 1) {
 			System.out.println("Could not reverse, add some elements!");
@@ -12,7 +29,7 @@ class LinkedList {
 		Node currNode = this.head;
 		Node prevNode = null;
 		Node nextNode = null;
-		
+
 		while (currNode != null) {
 			nextNode = currNode.next;
 			currNode.next = prevNode;
@@ -22,7 +39,7 @@ class LinkedList {
 		this.head = prevNode;
 		System.out.println("LIST REVERSED!");
 	}
-	
+
 	public void search(int element) {
 		Node tempHead = this.head;
 		int index = 0;
@@ -75,6 +92,7 @@ class LinkedList {
 			System.out.print(tempHead.data + "\t");
 			tempHead = tempHead.next;
 		}
+		System.out.println();
 	}
 
 	static class Node {
@@ -97,10 +115,12 @@ public class BasicImpl {
 		list.insertElement(2);
 		list.insertElement(3);
 		list.insertElement(4);
-		System.out.println();
 		list.printList();
-		System.out.println("\n");
 		System.out.println("Size of the list is: " + list.getSize());
+		list.reverseList();
+		list.printList();
+		list.deleteByIndex(1);
+		list.printList();
 		list.reverseList();
 		list.printList();
 	}
