bfd261774d74b1908727cde6b2d106816b431d75
sominwadhwa <sominwadhwa@gmail.com>
2019-01-07 15:01:13 +0530

    MergeTwoSortedLinkedLists

--- src/linkedlist/BasicImpl.java
+++ src/linkedlist/BasicImpl.java
@@ -5,7 +5,44 @@ import java.util.HashSet;
 class LinkedList {
 
 	Node head;
-	
+
+	public LinkedList mergeSortedLists(LinkedList list2) {
+
+		Node head1 = this.head;
+		Node head2 = list2.head;
+
+		LinkedList list = new LinkedList();
+
+		if (head1.data < head2.data) {
+			list.insertElement(head1.data);
+			head1 = head1.next;
+		} else {
+			list.insertElement(head2.data);
+			head2 = head2.next;
+		}
+
+		while (head1 != null && head2 != null) {
+			if (head1.data < head2.data) {
+				list.insertElement(head1.data);
+				head1 = head1.next;
+			} else {
+				list.insertElement(head2.data);
+				head2 = head2.next;
+			}
+		}
+
+		while (head1 != null) {
+			list.insertElement(head1.data);
+			head1 = head1.next;
+		}
+		while (head2 != null) {
+			list.insertElement(head2.data);
+			head2 = head2.next;
+		}
+
+		return list;
+	}
+
 	public void unionOfLists(LinkedList list_temp) {
 		Node currHead = this.head;
 		HashSet<Integer> set = new HashSet<Integer>();
@@ -21,22 +58,21 @@ class LinkedList {
 				currHead.next = new Node(tempHead.data);
 				tempHead = tempHead.next;
 				currHead = currHead.next;
-			}
-			else
+			} else
 				tempHead = tempHead.next;
 		}
 	}
-	
+
 	public void deleteMAfterN(int m, int n) {
-		
+
 		if (this.head == null)
 			return;
-		
+
 		Node fastNode = this.head;
 		Node prevNode = null;
 		int i = 0;
 		int j = 0;
-		
+
 		while (fastNode != null) {
 			while (i < m && fastNode != null) {
 				i++;
@@ -264,21 +300,20 @@ class LinkedList {
 		}
 		System.out.println("Element not found!");
 	}
-	
+
 	public int returnMiddleElement() {
 		int size = this.getSize();
 		Node curr = this.head;
 		if (size % 2 == 0) {
-			int index = (size / 2)  - 1;
+			int index = (size / 2) - 1;
 			int count = 0;
 			while (count != index) {
 				curr = curr.next;
 				count++;
 			}
 			return curr.data;
-		}
-		else {
-			int index = size/2;
+		} else {
+			int index = size / 2;
 			int count = 0;
 			while (count != index) {
 				curr = curr.next;
@@ -287,7 +322,7 @@ class LinkedList {
 			return curr.data;
 		}
 	}
-	
+
 	public int getSize() {
 		Node tempHead = this.head;
 		int count = 0;
@@ -314,26 +349,26 @@ class LinkedList {
 		tempHead.next = node;
 		System.out.println("Successfully inserted element at index: " + count);
 	}
-	
+
 	public void insertElementByIndex(int element, int index) {
 		if (index >= this.getSize() || this.head == null) {
 			System.out.println("Invalid entry!");
 			return;
 		}
-		
+
 		Node curr = this.head;
 		int i = 0;
-		
+
 		while (i != index) {
 			curr = curr.next;
 			i++;
 		}
-		
+
 		Node next = curr.next;
 		curr.next = new Node(element);
 		curr.next.next = next;
 	}
-	
+
 	public void printList() {
 		Node tempHead = this.head;
 		if (tempHead == null) {
@@ -364,17 +399,18 @@ public class BasicImpl {
 	public static void main(String[] args) {
 
 		LinkedList list = new LinkedList();
+		LinkedList list2 = new LinkedList();
 		list.insertElement(1);
-		list.insertElement(2);
 		list.insertElement(3);
-		list.insertElement(4);
 		list.insertElement(5);
-		list.insertElement(6);
 		list.insertElement(7);
-		list.insertElement(8);
 		list.insertElement(9);
-		list.printList();
-		
-		System.out.println(list.returnMiddleElement());
+		list2.insertElement(2);
+		list2.insertElement(4);
+		list2.insertElement(6);
+		list2.insertElement(8);
+
+		list.mergeSortedLists(list2).printList();
+
 	}
 }
