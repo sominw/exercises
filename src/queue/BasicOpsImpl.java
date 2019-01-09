5eefda5db6be559866be993c59a7052f347ada2e
sominwadhwa <sominwadhwa@gmail.com>
2019-01-09 15:04:31 +0530

    BasicOps on LinkedQueue

--- src/queue/BasicOpsImpl.java
+++ src/queue/BasicOpsImpl.java
@@ -1,24 +1,25 @@
 package queue;
 
+import java.util.Scanner;
+
 public class BasicOpsImpl {
 
 	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
 		BasicQueue<Integer> q = new BasicQueue<>();
-		q.enqueue(1);
-		q.enqueue(2);
-		q.enqueue(3);
-		q.enqueue(4);
-		
-		q.dequeue();
-		
-		q.enqueue(5);
-		
+
+		int data = scan.nextInt();
+		while (data != -1) {
+			q.enqueue(data);
+			data = scan.nextInt();
+		}
+
 		while (q.getSize() > 0) {
 			System.out.print(q.dequeue() + "\t");
 		}
-		
-		
 
+		scan.close();
 	}
 
 }
