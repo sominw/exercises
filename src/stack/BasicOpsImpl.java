847eb83ccfe6fcce532cfb51e5fe7e78ad81b88c
sominwadhwa <sominwadhwa@gmail.com>
2019-01-09 09:37:03 +0530

    stk using arry

--- src/stack/BasicOpsImpl.java
+++ src/stack/BasicOpsImpl.java
@@ -1,9 +1,16 @@
 package stack;
 
 public class BasicOpsImpl {
+	
+	public static void push() {}
+	
+	public static void pop() {}
+	
+	public static void isEmpty() {}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
