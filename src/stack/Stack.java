15868c951a3df45be1c2eb1d350fc35b296b2438
sominwadhwa <sominwadhwa@gmail.com>
2019-01-09 09:10:14 +0530

    Basic Stack Node

---
+++ src/stack/Stack.java
@@ -0,0 +1,12 @@
+package stack;
+
+public class Stack<T> {
+	T data;
+	Stack<T> next;
+	
+	public Stack(T data) {
+		// TODO Auto-generated constructor stub
+		this.data = data;
+		this.next = null;
+	}
+}
