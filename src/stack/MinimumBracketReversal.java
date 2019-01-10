c15d814a999efadaf53b1872b54b16e9fd40a4f7
sominwadhwa <sominwadhwa@gmail.com>
2019-01-10 12:30:30 +0530

    MinBracketReversalToBalance - Complete

--- src/stack/MinimumBracketReversal.java
+++ src/stack/MinimumBracketReversal.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class MinimumBracketReversal {
-	
+
 	public static int requiredMinimumBracketsToBalance(String exp) {
 		Stack<Character> stack = new Stack<>();
 		for (int i = 0; i < exp.length(); i++) {
 			char ch = exp.charAt(i);
 			if (ch == '{')
 				stack.push(ch);
-			else 
+			else
 				stack.pop();
 		}
 		if (stack.size() % 2 == 0)
-			return stack.size()/2;
+			return stack.size() / 2;
 		else
 			return -1;
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String exp = scan.nextLine();
