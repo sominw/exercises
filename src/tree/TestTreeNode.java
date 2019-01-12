068ec5779074df33385ae507a237972ee3adee5b
sominwadhwa <sominwadhwa@gmail.com>
2019-01-12 14:20:15 +0530

    Tested with debugger

--- src/tree/TestTreeNode.java
+++ src/tree/TestTreeNode.java
@@ -7,7 +7,9 @@ public class TestTreeNode {
 		TreeNode<Integer> node2 = new TreeNode<Integer>(12);
 		TreeNode<Integer> node3 = new TreeNode<Integer>(13);
 		TreeNode<Integer> node4 = new TreeNode<Integer>(14);
+		TreeNode<Integer> node5 = new TreeNode<Integer>(15);
 		node3.children.add(node4);
+		node3.children.add(node5);
 		root.children.add(node1);
 		root.children.add(node2);
 		root.children.add(node3);
