1135da4e135df1f35ce53f872f63f98e63431607
Somin Wadhwa <sominwadhwa@gmail.com>
2019-01-13 10:51:24 +0530

    Working test

--- src/tree/TestTreeNode.java
+++ src/tree/TestTreeNode.java
@@ -1,18 +1,36 @@
 package tree;
 
+import java.util.Scanner;
+
 public class TestTreeNode {
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static TreeNode<Integer> input() {
+		int data = scan.nextInt();
+		TreeNode<Integer> node = new TreeNode<Integer>(data);
+//		System.out.println("Enter number of children for this current node " + data + " :");
+		int childCount = scan.nextInt();
+		for (int i = 0; i < childCount; i++) {
+			TreeNode<Integer> childNode = input();
+			node.children.add(childNode);
+		}
+		return node;
+	}
+	
+	public static void printTreeNode(TreeNode<Integer> node) {
+		String s = node.data + ": ";
+		for (int i = 0; i < node.children.size(); i++) {
+			s = s + node.children.get(i).data + ", ";
+		}
+		System.out.println(s);
+		for (int i = 0; i < node.children.size(); i++) {
+			printTreeNode(node.children.get(i));
+		}
+	}
+
 	public static void main(String[] args) {
-		TreeNode<Integer> root = new TreeNode<Integer>(10);
-		TreeNode<Integer> node1 = new TreeNode<Integer>(11);
-		TreeNode<Integer> node2 = new TreeNode<Integer>(12);
-		TreeNode<Integer> node3 = new TreeNode<Integer>(13);
-		TreeNode<Integer> node4 = new TreeNode<Integer>(14);
-		TreeNode<Integer> node5 = new TreeNode<Integer>(15);
-		node3.children.add(node4);
-		node3.children.add(node5);
-		root.children.add(node1);
-		root.children.add(node2);
-		root.children.add(node3);
-		System.out.println(root.data);
+		TreeNode<Integer> root = input();
+		printTreeNode(root);
 	}
 }
