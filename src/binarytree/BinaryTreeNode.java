1bde409de600a91398b2ed25342da9cd90ed483d
sominwadhwa <sominwadhwa@gmail.com>
2019-01-15 11:31:23 +0530

    BinaryTreeNode - Complete

---
+++ src/binarytree/BinaryTreeNode.java
@@ -0,0 +1,12 @@
+package binarytree;
+
+public class BinaryTreeNode<T> {
+	public T data;
+	public BinaryTreeNode<T> left;
+	public BinaryTreeNode<T> right;
+	
+	public BinaryTreeNode(T data) {
+		// TODO Auto-generated constructor stub
+		this.data = data;
+	}
+}
