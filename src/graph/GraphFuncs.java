f59e5bb652d6811405b027f991ee90a045fb40f4
Somin Wadhwa <sominwadhwa@gmail.com>
2019-05-19 11:34:45 +0530

    IndentFix - Complete

--- src/graph/GraphFuncs.java
+++ src/graph/GraphFuncs.java
@@ -4,51 +4,51 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class GraphFuncs {
-	
+
 	private static void BFSUtil(Graph g, int node, boolean[] visited) {
-		
+
 		Queue<Integer> queue = new LinkedList<Integer>();
 		queue.add(node);
 		visited[node] = true;
-		
+
 		while (!queue.isEmpty()) {
 			int v = queue.remove();
- 			System.out.print(v +"\t");
+			System.out.print(v + "\t");
 			LinkedList<Integer> connections = g.adj[v];
-			for (Integer i: connections) {
+			for (Integer i : connections) {
 				if (visited[i] == false) {
 					queue.add(i);
 					visited[i] = true;
 				}
 			}
 		}
-		
+
 	}
-	
+
 	public static void BFS(Graph g) {
-		
+
 		boolean visited[] = new boolean[g.numNodes()];
-		
-		for(int i = 0; i < g.numNodes(); i++) {
+
+		for (int i = 0; i < g.numNodes(); i++) {
 			if (visited[i] == false) {
 				BFSUtil(g, i, visited);
 			}
 		}
-		
+
 	}
-	
+
 	private static void DFSUtil(Graph g, int node, boolean visited[]) {
 		visited[node] = true;
 		System.out.print(node + "\t");
 		LinkedList<Integer> connections = g.adj[node];
-		for (Integer i: connections) {
+		for (Integer i : connections) {
 			if (visited[i] == false)
 				DFSUtil(g, i, visited);
 		}
 	}
-	
+
 	public static void DFS(Graph g) {
-		
+
 		boolean visited[] = new boolean[g.numNodes()];
 
 		for (int i = 0; i < g.numNodes(); i++) {
@@ -58,7 +58,7 @@ public class GraphFuncs {
 	}
 
 	public static void main(String[] args) {
-		
+
 		Graph g = new Graph(8);
 		g.addEdge(0, 1);
 		g.addEdge(1, 2);
@@ -69,9 +69,9 @@ public class GraphFuncs {
 		g.addEdge(5, 6);
 		g.addEdge(6, 7);
 		g.addEdge(5, 7);
-		
+
 		BFS(g);
-		
+
 	}
 
 }
